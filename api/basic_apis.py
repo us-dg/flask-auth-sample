ffeca26d7b2e95c6c0a682e3e545acd6cfaad559
Utkarsh Singh <us9044@gmail.com>
2022-05-31 17:20:52 +0530

    Added route to check headers

--- api/basic_apis.py
+++ api/basic_apis.py
@@ -1,7 +1,14 @@
 import json 
+from flask import request
 
 def heartbeat():
     response = {
         "msg": "Hello World!"
     }
     return response 
+
+def headers():
+    output = dict()
+    for i in request.headers.items():
+        output[i[0]] = i[1]
+    return output
